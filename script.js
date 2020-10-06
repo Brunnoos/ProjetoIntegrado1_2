7c99a83f709457a1d4948dd21461b422ea5d7dfd
Bruno Oliveira <brunno.os159@gmail.com>
2020-10-06 20:41:10 -0300

    Sistema de Filtro (wip)

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 ﻿var inst = [];
-var list_bairro = []
+var list_bairro = [];
+
+var inst_filters = {};
+
+var filterList = {};
+var filterApplied = []; /* True sempre que qualquer filtro esteja ativo */
 
 $.get("data.json",
     function (data) {
@@ -53,8 +58,18 @@ $.get("data.json",
         
         for (let i = 0; i < list_bairro.length; i++) {
             const element = list_bairro[i];
-
+            
             let list_inst = "";
+
+            for (var key in element.list[0]) {
+                var newList = [];
+                if (key != "nome" && key != "horario" && key != "link_map" && key != "numero" && key != "bairro" && key != "docs"){
+                    if (!(key in inst_filters)) {
+                        inst_filters[key.toLowerCase()] = newList;
+                    }
+                }
+            }
+
             for (let j = 0; j < element.list.length; j++) {
                 list_inst += 
                 "<div class=\"local-ref\">"+
@@ -62,8 +77,15 @@ $.get("data.json",
                     "<div class=\"local-ref-info\" name=\""+element.list[j].nome+"\"></div>"+
                     "<a href=\"" + element.list[j].link_map + "\" target=\"_blank\"><div class=\"local-ref-map\"></div> </a>"+
                 "</div>";
+
+                for (var key in inst_filters) {
+                    console.log(element.list[j][key] + " | " + inst_filters[key].indexOf(element.list[j][key]));
+                    if (inst_filters[key].indexOf(element.list[j][key].toLowerCase()) == -1) {
+                        inst_filters[key].push(element.list[j][key].toLowerCase());
+                    }
+                }
             }
-            
+
             $('#bairro-list').append(
                 "<div class=\"bairro-block\">"+
                     "<div class=\"bairro-header\">"+
@@ -85,11 +107,16 @@ $.get("data.json",
                 generatePlantao(inst[x]);
             }
         }
+
+        for (var key in inst_filters) {
+            inst_filters[key].push("tudo");
+        }
+        console.log(inst_filters);
+        createFilterList();
+
         AddClickEvent();
     }
 );
-
-
 
 
 function AddClickEvent() {
@@ -109,6 +136,10 @@ function AddClickEvent() {
 
     $("#filter-button").on("click", openFilterTab);
     $("#close-filter-tab").on("click", closeFilterTab);
+
+    $(".filter-toggle").each(function(){
+        $(this).on("click", filterSelect);
+    });
 }
 
 function toggleBairroList(){
@@ -174,6 +205,196 @@ function closeFilterTab() {
     filterTab.css("animation-name", "closeFilterTab");
     setTimeout(function(){ filterTabBg.css("display", "none"); }, 500);
 };
+
+function createFilterList() {
+    for (var key in inst_filters) {
+        var newFilterSubList = [];
+
+        for (let i = 0; i < inst_filters[key].length - 1; i++) {
+            newFilterSubList.push(false);
+        }
+
+        newFilterSubList.push(true); /* Opção para Todos */
+
+        filterList[key] = newFilterSubList;
+    }
+
+    console.log(filterList);
+
+    for (var key in filterList) {
+        switch(key) {
+            case "preco":                                     /* Preços */
+                $("#filters-list").append(
+                    "<div class=\"filters\" id=\"filterType-" + key + "\">" + 
+                        "<p class=\"filters-title\">Preços</p>" +
+                    "</div>"
+                );
+                
+                break;
+
+            case "atendimento":                                     /* Atendimento */
+                $("#filters-list").append(
+                    "<div class=\"filters\" id=\"filterType-" + key + "\">" + 
+                        "<p class=\"filters-title\">Atendimento</p>" +
+                    "</div>"
+                );
+                
+                break;
+        }
+     }
+    for (var key in inst_filters) {
+        for (let i = 0; i < inst_filters[key].length; i++) {
+            $("#filterType-" + key).append(
+                "<div class=\"filter-option\" type=\"" + key + "\" value=\"" + inst_filters[key][i] + "\">" + 
+                    inst_filters[key][i].substring(0,1).toUpperCase() + inst_filters[key][i].substring(1) +
+                "</div>" +
+                "<div class=\"filter-toggle\"><div class=\"filter-toggle-icon\"></div></div>"
+            );
+        }
+    }
+}
+
+function filterSelect() {
+    var filterType = $(this).prev(".filter-option").attr("type");
+    var filter = $(this).prev(".filter-option").attr("value");
+    var filterIndex = inst_filters[filterType].indexOf(filter);
+    console.log(filterIndex);
+
+    var filterTypeSize = filterList[filterType].length - 1; /* Exclui o último, que equivale a opção Tudo */
+    var filterTypeCount = 0;
+
+    if (filterList[filterType][filterIndex] == true) {
+        filterList[filterType][filterIndex] = false;
+    } else {
+        filterList[filterType][filterIndex] = true;
+    }
+    
+    if (filterIndex == filterTypeSize) {
+        for (var i = 0; i < filterTypeSize; i++) {
+            filterList[filterType][i] = false;
+        }
+    
+        showAllElements();
+        checkBairroList();
+    }
+    else {
+        for (var i = 0; i < filterTypeSize; i++) {
+            if (filterList[filterType][i] == true) {
+                filterTypeCount++;
+            }
+        }
+
+        if (filterTypeCount == filterTypeSize) {
+            for (var i = 0; i < filterTypeSize; i++) {
+                filterList[filterType][i] = false;
+            }
+        
+            filterList[filterType][filterTypeSize] = true;
+            showAllElements();
+
+            checkBairroList();
+        }
+        else {
+            
+            filterList[filterType][filterTypeSize] = false;
+            if (filterApplied != true) {
+                hideAllElements();
+
+                for (let i = 0; i < inst.length; i++) {
+                    var element = inst[i];
+
+                    if (element[filterType].toLowerCase() == inst_filters[filterType][filterIndex]) {
+                        showElement(element.nome);
+                    }
+                }
+            }
+            else {
+                for (let i = 0; i < inst.length; i++) {
+                    var element = inst[i];
+
+                    if (element[filterType].toLowerCase() != inst_filters[filterType][filterIndex]) {
+                        hideElement(element.nome);
+                    }
+                }
+            }
+        
+            checkBairroList();
+        }
+        console.log(filterList[ filterType ]);
+    }
+};
+
+function reApplyFilter(){
+    for (var key in filterList) {
+        for (let i = 0; i < filterList[key].length; i++) {
+            if (filterList[key][i] == true) {
+                for (let j = 0; j < inst.length; j++) {
+                    var element = inst[j];
+
+                    if (element[filterType].toLowerCase() != inst_filters[filterType][filterIndex]) {
+                        hideElement(element.nome);
+                    }
+                }
+            }
+        }
+    }
+}
+
+function hideElement(nome) {
+    $(".local-ref").each(function(){
+        if ($(this).find(".local-ref-info").attr("name") == nome)
+        {
+            $(this).css("display", "none");
+            $(this).attr("state", "hidden");
+        }
+    });    
+};
+
+function showElement(nome) {
+    $(".local-ref").each(function(){
+        if ($(this).find(".local-ref-info").attr("name") == nome)
+        {
+            $(this).css("display", "flex");
+        }
+    }); 
+};
+
+function hideAllElements() {
+    $(".local-ref").each(function(){
+        $(this).css("display", "none");
+    });  
+    
+    filterApplied = true;
+}
+
+function showAllElements() {
+    $(".local-ref").each(function(){
+        $(this).css("display", "flex");
+    });  
+    
+    filterApplied = false;
+}
+
+function checkBairroList(){
+    $(".bairro-locais-list").each(function(){
+        var locaisEscondidos = 0;
+
+        var locais = $(this).find(".local-ref").length;
+
+        for (let i = 1; i <= locais; i++) {
+            if ($(this).find(".local-ref:nth-child(" + i + ")").css("display") == "none") {
+                locaisEscondidos++;
+            }
+        }
+
+        if (locaisEscondidos == locais) {
+            $(this).parent().css("display","none");
+        }
+        else {
+            $(this).parent().css("display","block");
+        }
+    });
+}
 
 function openLocalInfo(elem) {
     console.log($(elem.target).attr('name'));
